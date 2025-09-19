6b87ce04ee8c576ee6ebaf14016cc2ee0c990477
jiangpeng <k9sy@qq.com>
2025-09-19 17:30:00 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,9 @@
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import vercel from '@astrojs/vercel'
 import AstroPureIntegration from 'astro-pure'
 import { defineConfig } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
 
-// Others
-// import { visualizer } from 'rollup-plugin-visualizer'
-
-// Local integrations
-// Local rehype & remark plugins
 import rehypeAutolinkHeadings from './src/plugins/rehype-auto-link-headings.ts'
 // Shiki
 import {
@@ -22,29 +16,15 @@ import {
 } from './src/plugins/shiki-transformers.ts'
 import config from './src/site.config.ts'
 
-// https://astro.build/config
 export default defineConfig({
   // Disable dev toolbar
   devToolbar: {
     enabled: false
   },
-  // Top-Level Options
   site: 'https://pengJiang0930.github.io',
-  // Deploy to a sub path; See https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
-  base: '/blog/',
   trailingSlash: 'never',
-
-  // Adapter
-  // https://docs.astro.build/en/guides/deploy/
-  // 1. Vercel (serverless)
-  adapter: vercel(),
-  output: 'server',
-  // 2. Vercel (static)
-  // adapter: vercelStatic(),
-  // 3. Local (standalone)
-  // adapter: node({ mode: 'standalone' }),
-  // output: 'server',
-  // ---
+  output: 'static',
+  
 
   image: {
     responsiveStyles: true,
@@ -54,27 +34,14 @@ export default defineConfig({
   },
 
   integrations: [
-    // astro-pure will automatically add sitemap, mdx & unocss
-    // sitemap(),
-    // mdx(),
+  
     AstroPureIntegration(config)
-    // (await import('@playform/compress')).default({
-    //   SVG: false,
-    //   Exclude: ['index.*.js']
-    // }),
-
-    // Temporary fix vercel adapter
-    // static build method is not needed
+   
   ],
-  // root: './my-project-directory',
-
-  // Prefetch Options
   prefetch: true,
-  // Server Options
   server: {
     host: true
   },
-  // Markdown Options
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [
@@ -89,7 +56,6 @@ export default defineConfig({
         }
       ]
     ],
-    // https://docs.astro.build/en/guides/syntax-highlighting/
     shikiConfig: {
       themes: {
         light: 'github-light',
@@ -110,10 +76,7 @@ export default defineConfig({
   },
   vite: {
     plugins: [
-      //   visualizer({
-      //     emitFile: true,
-      //     filename: 'stats.html'
-      //   })
+
     ]
   }
 })
